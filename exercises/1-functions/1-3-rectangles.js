c3a6f90a2dea7d91015939466633a10781580eef
Ernest Marot <ernest.marot@student.ehb.be>
2022-10-07 15:41:05 +0200

    simplied code (less repetiton)

--- exercises/1-functions/1-3-rectangles.js
+++ exercises/1-functions/1-3-rectangles.js
@@ -11,18 +11,15 @@ drawCrossedBox();
 
 function drawCrossedBox() {
 
-    //upper left rect
+    //white rect w/ red borders
     context.strokeStyle = "red";
+    context.fillStyle = "white";
     context.beginPath();
     context.rect(50, 50, 150, 150);
-    context.fill;
-    context.stroke();
-
-    //lower right rect
-    context.strokeStyle = "red";
-    context.beginPath();
     context.rect(200, 200, 150, 150);
-    context.fill;
+    context.rect(75, 275, 50, 50);
+    context.rect(275, 75, 50, 50);
+    context.fill();
     context.stroke();
 
     //black middle rect
@@ -33,31 +30,10 @@ function drawCrossedBox() {
     context.fill();
     context.stroke();
 
-    //lower left white rect
-    context.strokeStyle = "red";
-    context.fillStyle = "white";
-    context.beginPath();
-    context.rect(75, 275, 50, 50);
-    context.fill();
-    context.stroke();
-
-    //lower left black rect
+    //small black rect
     context.fillStyle = "black";
     context.beginPath();
     context.rect(50, 325, 25, 25);
-    context.fill();
-
-    //upper right white rect
-    context.strokeStyle = "red";
-    context.fillStyle = "white";
-    context.beginPath();
-    context.rect(275, 75, 50, 50);
-    context.fill();
-    context.stroke();
-
-    //lower right black rect
-    context.fillStyle = "black";
-    context.beginPath();
     context.rect(325, 50, 25, 25);
     context.fill();
 
