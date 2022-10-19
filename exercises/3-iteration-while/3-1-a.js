dbf4fbe576071d60fded0a55c57609b8f464ba0b
Ernest Marot <ernest.marot@student.ehb.be>
2022-10-19 15:00:14 +0200

    3-1-c

--- exercises/3-iteration-while/3-1-a.js
+++ exercises/3-iteration-while/3-1-a.js
@@ -20,7 +20,7 @@ function drawLines() {
         //Add it to the utils script:
         //The function draws a line between 2 points
         //And has the following parameters: x1, y1, x2, y2
-        Utils.drawLine(80, margin + (i * 25), 320, margin + (i * 25));
+        Utils.drawLine(75, margin + (i * 25), 320, margin + (i * 25));
         i++;
     }
 }
